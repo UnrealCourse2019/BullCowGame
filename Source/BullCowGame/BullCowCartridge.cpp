af8f004e450a4d37deacd4d300b68842402f63ad
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-06-02 13:16:40 +0100

    6_BC_UC2 FStrings

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,9 +5,11 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bulls & Cows!"));
+    PrintLine(TEXT("Try guess the 4 letter hidden word..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+    FString HiddenWord = TEXT("Cake");
 }
