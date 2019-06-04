3225fbd3429d1d9d444f87554c7e7e0e4566ae9e
Gavin Milroy <gavinmilroy@tesla-dev.com>
2019-06-04 15:34:53 +0100

    8_BC_UC2 String Formatting With Printf

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,24 +1,25 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+FString HiddenWord = TEXT("Unreal");
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bulls & Cows!"));
-    PrintLine(TEXT("Try guess the 4 letter hidden word..."));
+    PrintLine(FString::Printf(TEXT("Try guess the %i letter word"), HiddenWord.Len()));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("Cake");
+
     if (Input.Len() == HiddenWord.Len())
     {
         PrintLine(TEXT("Correct amount of letters!"));
     }
     else
     {
-        PrintLine(TEXT("You need to enter a 4 letter word!"));
+        PrintLine(FString::Printf(TEXT("You must enter a %i letter word!"), HiddenWord.Len()));
     }
-    
 }
