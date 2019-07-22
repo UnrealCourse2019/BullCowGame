46fa5739750d4209a3ec06d2bad8b52a9578361c
Mikey <mike@michaelbridges.co.uk>
2019-07-22 20:42:03 +0100

    11 Using The In Game Terminal

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,11 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to Bull Cows!");
+    PrintLine("Press enter to continue...");
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
