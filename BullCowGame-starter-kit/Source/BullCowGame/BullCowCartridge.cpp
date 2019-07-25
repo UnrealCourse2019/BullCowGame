a3ad2ad4ae944a19345c063e21ceaeceaf96c0a0
Michael Bridges <mike@michaelbridges.co.uk>
2019-07-25 11:03:56 +0100

    13 The TEXT Macro

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,12 +4,12 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to Bull Cows!");
-    PrintLine("Press enter to continue...");
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "cake";
+    FString HiddenWord = TEXT("cake");
 }
