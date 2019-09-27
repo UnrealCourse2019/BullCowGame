3d6c2c48c4111abe210536d484926c4c758a0775
Michael Bridges <mike@michaelbridges.co.uk>
2019-09-27 12:28:46 +0100

    34 TArray Of Hidden Words

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWordList.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
---
+++ BullCowGame-starter-kit/Source/BullCowGame/HiddenWordList.h
@@ -0,0 +1,10 @@
+#include "CoreMinimal.h"
+
+const TArray<FString> Words =
+{
+    TEXT("cakes"),
+    TEXT("water"),
+    TEXT("plums"),
+    TEXT("kings"),
+    TEXT("knife")
+};
