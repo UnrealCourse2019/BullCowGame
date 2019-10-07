3047a2f9cf24bfd24f19ca0057d3c29fa8ae12f4
Michael Bridges <mike@michaelbridges.co.uk>
2019-10-07 16:57:29 +0100

    37 Logical Operators

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -11,9 +11,12 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("The number of possible words is %i."), Words.Num());
     PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord);// Debug Line   
 
-    for (int32 Index = 0; Index != 5; Index++)
+    for (int32 Index = 0; Index != 10; Index++)
     {
-        PrintLine(TEXT("%s"), *Words[Index]);
+        if (Words[Index].Len() >=4 && Words[Index].Len() <=8)
+        {
+            PrintLine(TEXT("%s"), *Words[Index]);
+        }
     }
     
 }
