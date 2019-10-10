b26cb976478ead759fe5db4e8f3a7af5ea11c135
Mikey <mike@michaelbridges.co.uk>
2019-10-10 09:57:40 +0100

    38 TArray Functions Adding And Removing

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -9,16 +9,8 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     SetupGame();
 
     PrintLine(TEXT("The number of possible words is %i."), Words.Num());
-    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord);// Debug Line   
-
-    for (int32 Index = 0; Index != 10; Index++)
-    {
-        if (Words[Index].Len() >=4 && Words[Index].Len() <=8)
-        {
-            PrintLine(TEXT("%s"), *Words[Index]);
-        }
-    }
-    
+    PrintLine(TEXT("The number of valid words is: %i."), GetValidWords(Words).Num());
+    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord);// Debug Line     
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -110,4 +102,18 @@ bool UBullCowCartridge::IsIsogram(FString Word) const
     }
 
     return true;
+}
+
+TArray<FString> UBullCowCartridge::GetValidWords(TArray<FString> WordList) const
+{
+    TArray<FString> ValidWords;
+    
+    for (int32 Index = 0; Index < WordList.Num(); Index++)
+    {
+        if (WordList[Index].Len() >= 4 && WordList[Index].Len() <= 8 && IsIsogram(WordList[Index]))
+        {
+                ValidWords.Emplace(WordList[Index]);      
+        }
+    }
+    return ValidWords;
 }
--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -18,6 +18,7 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	void EndGame();
 	void ProcessGuess(FString Guess);
 	bool IsIsogram(FString Word) const;
+	TArray<FString> GetValidWords(TArray<FString>) const;
 
 	// Your declarations go below!
 	private:
